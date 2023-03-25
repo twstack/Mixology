22fd13471f47dd52854151ff98e908f9568571b8
Thomas Stack <stackta@bc.edu>
2023-03-25 13:56:27 -0400

    Initial Commit

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Thomas Stack on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
